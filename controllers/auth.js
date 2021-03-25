1d32f6fefffa3838eea2e2141008a0f35366952e
enel <enel@gmail.com>
2021-03-25 21:52:16 +0000

    finisged backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
+const crypto = require("crypto");
 const User = require("../models/User");
 const ErrorResponse = require("../utils/errorResponse");
-
+const sendEmail = require("../utils/sendEmail");
 //register Route
 exports.register = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -65,12 +66,52 @@ exports.forgotpassword = async (req, res, next) => {
     `;
 
     try {
-    } catch (error) {}
-  } catch (error) {}
+      await sendEmail({
+        to: user.email,
+        subject: "Password Reset Request",
+        text: message,
+      });
+      res.status(200).json({ success: true, data: "Email Sent" });
+    } catch (error) {
+      (user.resetpasswordToken = undefined),
+        (user.resetpasswordExpire = undefined),
+        await user.save();
+
+      return next(new ErrorResponse("Email could not be sent", 500));
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
-exports.resetpassword = (req, res, next) => {
-  res.send("Reset Password route");
+exports.resetpassword = async (req, res, next) => {
+  const resetpasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.resetToken)
+    .digest("hex");
+
+  try {
+    const user = await User.findOne({
+      resetpasswordToken,
+      resetpasswordExpire: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return next(new ErrorResponse("invalid reset token ", 400));
+    }
+
+    user.password = req.body.password;
+    user.resetpasswordToken = undefined;
+    user.resetpasswordExpire = undefined;
+
+    await user.save();
+    res.status(201).json({
+      success: true,
+      data: "Password reset Success",
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const sendToken = (user, statusCode, res) => {
